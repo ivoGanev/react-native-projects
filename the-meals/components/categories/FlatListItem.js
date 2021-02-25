d010032e017b6c00dc2a0f82771700f40c684178
Ivo Ganev <ivo.ganev@yahoo.com>
2021-02-25 11:32:33 +0000

    Style grid elements and refactor

--- the-meals/components/categories/FlatListItem.js
+++ the-meals/components/categories/FlatListItem.js
@@ -1,12 +1,59 @@
 import React from 'react'
-import { View, Text, TouchableHighlight, ProgressViewIOSComponent } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet, Platform, TouchableNativeFeedback } from 'react-native'
+
+
 
 export const ListItem = props => {
+    if (Platform.OS === 'android' && Platform.Version >= 21) {
+        return (
+            <View  style={styles.gridItem}>
+                <TouchableNativeFeedback
+                    onPress={
+                        props.onPress
+                    }
+                   style = {{flex:1}}
+                    background={TouchableNativeFeedback.Ripple('#FFFFFF', true)}
+                >
+                    <View style={{ ...styles.container, ...{ backgroundColor: props.itemData.color } }}>
+                        <Text style={styles.text} >{props.itemData.title}</Text>
+                    </View>
+                </TouchableNativeFeedback>
+            </View>)
+    }
+
     return (
-        <TouchableHighlight onPress={ props.onPress }>
-            <View>
-                <Text>{props.itemData.title}</Text>
+        <TouchableOpacity onPress={props.onPress} style={styles.gridItem}>
+            <View style={{ ...styles.container, ...{ backgroundColor: props.itemData.color } }}>
+                <Text style={styles.text} >{props.itemData.title}</Text>
             </View>
-        </TouchableHighlight>
+        </TouchableOpacity>
     )
 }
+
+const styles = StyleSheet.create({
+    gridItem: {
+        flex: 1,
+        margin: 15,
+        elevation: 3,
+        borderRadius: 10,
+        shadowColor: 'black',
+        shadowOpacity: 0.26,
+        shadowRadius: 10,
+        shadowOffset: { width: 0, height: 2 },
+    },
+
+    container: {
+        flex: 1,
+        height: 150,
+        borderRadius: 10,
+        padding: 10,
+        justifyContent: 'flex-end',
+        alignContent: 'flex-end',
+        flexDirection: 'column',
+    },
+
+    text: {
+        textAlign: 'right',
+        fontFamily: 'open-sans-bold',
+    }
+})
